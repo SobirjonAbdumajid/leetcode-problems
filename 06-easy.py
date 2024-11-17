f1c572c458d7e36768493110ad227fb42fff777e
Sobirjon005 <sobirjon0305@gmail.com>
2024-11-17 14:40:35 +0500

    problem 5

---
+++ 06-easy.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+
+
+M = 1000
+CM = 900
+XC = 90
+IV = 4
+III = 3
+L = 50
+V = 5 
+III = 3
+print(III)
