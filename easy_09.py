98a9d35d8828fe0545b0adb3aebb14d666333051
Sobirjon005 <sobirjon0305@gmail.com>
2025-02-17 18:11:18 +0500

    canConstruct is solved

--- easy_09.py
+++ easy_09.py
@@ -1,17 +1,40 @@
+# 1
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
-        """
-        :type ransomNote: str
-        :type magazine: str
-        :rtype: bool
-        """
-        print(set(ransomNote), len(set(ransomNote)), set(magazine), len(set()))
-        if ransomNote == magazine or len(ransomNote) == len(magazine) and len(set(ransomNote)) == len(set(magazine)):
+        ransomNote_ = list(ransomNote)
+        magazine_ = list(magazine)
+        counter = 0
+        for i in ransomNote_:
+            if i in magazine_:
+                magazine_.remove(i)
+                counter += 1
+        if counter == len(ransomNote_):
             return True
         return False
 
 
+# # 2
+# class Solution(object):
+#     def canConstruct(self, ransomNote, magazine):
+#         d1 = {}
+#         d2 = {}
+#
+#         A = sorted(list(set(ransomNote)))
+#         B = sorted(list(set(magazine)))
+#         for i in A:
+#             if i not in B:
+#                 return False
+#         for i in ransomNote:
+#             d1[i] = d1.get(i, 0) + 1
+#         for i in magazine:
+#             d2[i] = d2.get(i, 0) + 1
+#         for i, j in d1.items():
+#             if d1[i] > d2[i]:
+#                 return False
+#         return True
+
+
 sol = Solution()
-ransomNote = "aa"
-magazine = "aab"
+ransomNote = "aab"
+magazine = "aasgb"
 print(sol.canConstruct(ransomNote, magazine))
