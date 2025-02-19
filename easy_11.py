225cd0676cf380c98ad13c84ab5fa315d7dedeab
Sobirjon005 <sobirjon0305@gmail.com>
2025-02-19 14:04:52 +0500

    maxNumberOfBalloons is done

--- easy_11.py
+++ easy_11.py
@@ -1,35 +1,12 @@
 class Solution(object):
     def maxNumberOfBalloons(self, text):
-        total_text_count = 0
-        total_balloon_count = 0
+        _set = set()
 
         balloon = "balloon"
         for i in "balon":
-            total_text_count += text.count(i)
-            total_balloon_count += balloon.count(i)
-            print(i, total_text_count, total_balloon_count)
-
-        # return total_text_count // total_balloon_count
-            # if True:
-            # print(i, text.count(i), balloon.count(i))
-        # return 0
-
-
-        # list_text = list(text)
-        # counter = 0
-        # balloon = "balloon"
-        # for i in balloon:
-        #     for j in text:
-        #         if i == j:
-        #             counter += 1
-        #             print(counter)
-        #             # try:
-        #             list_text.remove(j)
-        #             # except:
-        #             #     counter -= 1
-        # return counter // len(balloon)
+            _set.add(text.count(i) // balloon.count(i))
+        return min(_set)
 
 
 s = Solution()
-print(s.maxNumberOfBalloons("loonbalxballllllllpoon"))
-
+print(s.maxNumberOfBalloons("loonbalxballlpoon"))
