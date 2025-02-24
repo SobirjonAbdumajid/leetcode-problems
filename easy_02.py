1bd78945c4e0dbc08c8b642a35dcdccf8e2cb7c4
Sobirjon005 <sobirjon0305@gmail.com>
2025-02-24 09:47:04 +0500

    twoSum is done

--- easy_02.py
+++ easy_02.py
@@ -81,7 +81,6 @@
 #                 return [i, nums.index(target - num)]
 
 
-
 class Solution(object):
     def twoSum(self, nums, target):
         _dict = {}
@@ -98,4 +97,3 @@ print(solution.twoSum([3, 2, 3], 6))
 print(solution.twoSum([3, 2, 4], 6))
 print(solution.twoSum([2, 7, 11, 15], 9))
 print(solution.twoSum([1, 2, 3, 4, 5], 9))
-
