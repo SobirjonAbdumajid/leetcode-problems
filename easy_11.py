0646e0b8bebe6558fb45c1243097c76f9e30ede8
Sobirjon005 <sobirjon0305@gmail.com>
2025-02-24 09:47:47 +0500

    maxNumberOfBalloons is updated

--- easy_11.py
+++ easy_11.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def maxNumberOfBalloons(self, text):
         _set = set()
-
         for i in "balon":
             _set.add(text.count(i) // "balloon".count(i))
         return min(_set)
